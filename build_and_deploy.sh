74e925b3224492c6acda37584757cceff726b0f5
Zain Baquar <zain.baq@gmail.com>
2023-03-23 21:52:09 -0400

    changed spacing in body container

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 # remove old build
+echo "Removing old build"
 rm -rf ./build
 
 # Pull master branch
+echo "Pulling latest updates"
 git pull origin master
 
 # npm build and deploy
+echo "Deploying..."
 npm run deploy -- -m "Deploy React app to GitHub Pages"
 
